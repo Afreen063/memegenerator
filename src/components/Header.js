69a2e2864805ba05cdbf0ab5ac92b08fb51d118d
Afreen063 <afreen0501@gmail.com>
2022-06-26 00:19:01 +0530

    header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header()
+{
+    return (
+        <header className="header">
+            <img src="./images/logo.jpg"/>
+            <h2>MemeGenerator</h2>
+            <h4>React-Project 3</h4>
+        </header>
+    )
+}
