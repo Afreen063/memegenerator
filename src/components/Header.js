f196e35fb563e01b4eb32056056d5e944aa69d6b
Afreen063 <afreen0501@gmail.com>
2022-06-28 21:34:17 +0530

    problem while using images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+
 export default function Header()
 {
     return (
         <header className="header">
-            <img src="./images/logo.jpg"/>
+            <img src=""/>
             <h2>MemeGenerator</h2>
             <h4>React-Project 3</h4>
         </header>
