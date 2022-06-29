907d942389ea049a8ef37b6bd7565f122e7d78a4
Afreen063 <afreen0501@gmail.com>
2022-06-29 18:14:01 +0530

    use of React.useState

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -2,20 +2,21 @@ import React from "react";
 
 export default function Greet(prop)
 {
-    const date=new Date();
-    const hours= date.getHours();
-    let g="";
-    if(hours>=12)
-    g="morning";
-    else
-     g="evening";
-     const h=React.useState(prop.name);//useState is basically an array
-     console.log(h);
-    return (
-        <h1>{h[0]} {g} {prop.name}</h1>
-    )
+   const [isImportant, setIsImportant]= React.useState("Yes");
+   function handleClick()
+   {
+             setIsImportant("No");
+   }
+   return (
+    <div>
+        <h1>is state important? {isImportant}</h1>
+        <button onClick={handleClick}>click me</button>
+    </div>
+   )
 }
 //difference between state and prop in a component.
 // prop are immutable where state is mutable
 //g state is changing but the prop is not changing.
-// a state in react 
+// useState is react lib help us to change the state.
+//line 5- we are declaring an array with fisrt value as const and second as func.
+//the setIsImportant is use to set the value of isimportant.
